6c9fc61ad07372f0664f53297d48f3ca709e6908
Marc Tuduri <marc.tuduri@soundcloud.com>
2021-07-16 16:27:08 +0200

    Renaming

--- lib/periskop/client/collector.rb
+++ lib/periskop/client/collector.rb
@@ -6,18 +6,34 @@ module Periskop
   module Client
     class ExceptionCollector
       def initialize
-        @aggregated_exceptions = {}
+        @aggregated_exceptions_dict = {}
         @uuid = SecureRandom.uuid
       end
 
+      attr_reader :aggregated_exceptions_dict
+
       def aggregated_exceptions
-        Payload.new(@aggregated_exceptions.values, @uuid)
+        Payload.new(@aggregated_exceptions_dict.values, @uuid)
       end
 
       # Report an exception
       # Params:
       # exception:: captured exception
       def report(exception)
+        add_exception(exception, nil)
+      end
+
+      # Report an exception with context
+      # Params:
+      # exception:: captured exception
+      # context:: HTTP context of the exception
+      def report_with_context(exception, context)
+        add_exception(exception, context)
+      end
+
+      private
+
+      def add_exception(exception, context)
         exception_instance = ExceptionInstance.new(
           exception.class.name,
           exception.message,
@@ -26,19 +42,19 @@ module Periskop
         )
         exception_with_context = ExceptionWithContext.new(
           exception_instance,
-          nil,
+          context,
           'error'
         )
         aggregation_key = exception_with_context.aggregation_key()
 
-        unless @aggregated_exceptions.key?(aggregation_key)
+        unless @aggregated_exceptions_dict.key?(aggregation_key)
           aggregated_exception = AggregatedException.new(
             aggregation_key,
             'error'
           )
-          @aggregated_exceptions.store(aggregation_key, aggregated_exception)
+          @aggregated_exceptions_dict.store(aggregation_key, aggregated_exception)
         end
-        aggregated_exception = @aggregated_exceptions[aggregation_key]
+        aggregated_exception = @aggregated_exceptions_dict[aggregation_key]
         aggregated_exception.add_exception(exception_with_context)
       end
     end
