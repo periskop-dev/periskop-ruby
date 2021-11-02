510b04db2e7ea8a92dcf5ed26c4cdb181e06a1b9
Marc Tuduri <marc.tuduri@soundcloud.com>
2021-11-02 17:29:52 +0100

    Fix indentation

--- lib/periskop/rack/middleware.rb
+++ lib/periskop/rack/middleware.rb
@@ -25,62 +25,62 @@ module Periskop
 
         response
       end
-    end
 
-    # Web framework middlewares often store rescued exceptions inside the
-    # Rack env, but Rack doesn't have a standard key for it:
-    #
-    # - Rails uses action_dispatch.exception: https://goo.gl/Kd694n
-    # - Sinatra uses sinatra.error: https://goo.gl/LLkVL9
-    # - Goliath uses rack.exception: https://goo.gl/i7e1nA
-    def framework_exception(env)
-      env['rack.exception'] ||
-        env['sinatra.error'] ||
-        env['action_dispatch.exception']
-    end
-
-    def find_request(env)
-      if defined?(ActionDispatch::Request)
-        ActionDispatch::Request.new(env)
-      elsif defined?(Sinatra::Request)
-        Sinatra::Request.new(env)
-      else
-        ::Rack::Request.new(env)
-      end
-    end
-
-    def get_http_headers(request_env)
-      header_prefixes = %w[
-        HTTP_
-        CONTENT_TYPE
-        CONTENT_LENGTH
-      ].freeze
-
-      request_env.map.with_object({}) do |(key, value), headers|
-        if header_prefixes.any? { |prefix| key.to_s.start_with?(prefix) }
-          headers[key] = value
-        end
+      # Web framework middlewares often store rescued exceptions inside the
+      # Rack env, but Rack doesn't have a standard key for it:
+      #
+      # - Rails uses action_dispatch.exception: https://goo.gl/Kd694n
+      # - Sinatra uses sinatra.error: https://goo.gl/LLkVL9
+      # - Goliath uses rack.exception: https://goo.gl/i7e1nA
+      def framework_exception(env)
+        env['rack.exception'] ||
+          env['sinatra.error'] ||
+          env['action_dispatch.exception']
       end
 
-      headers
-    end
-
-    def get_http_context(env)
-      request = find_request(env)
-
-      Periskop::Client::HTTPContext.new(request.request_method, request.url, get_http_headers(request.env), nil)
-    end
-
-    def report_push(env, maybe_ex)
-      ex =
-        if maybe_ex.is_a?(Exception)
-          maybe_ex
+      def find_request(env)
+        if defined?(ActionDispatch::Request)
+          ActionDispatch::Request.new(env)
+        elsif defined?(Sinatra::Request)
+          Sinatra::Request.new(env)
         else
-          RuntimeError.new(maybe_ex.to_s)
+          ::Rack::Request.new(env)
+        end
+      end
+
+      def get_http_headers(request_env)
+        header_prefixes = %w[
+          HTTP_
+          CONTENT_TYPE
+          CONTENT_LENGTH
+        ].freeze
+
+        request_env.map.with_object({}) do |(key, value), headers|
+          if header_prefixes.any? { |prefix| key.to_s.start_with?(prefix) }
+            headers[key] = value
+          end
         end
 
-      @collector.report_with_context(env, ex)
-      @exporter.push_to_gateway(@pushgateway_address)
+        headers
+      end
+
+      def get_http_context(env)
+        request = find_request(env)
+
+        Periskop::Client::HTTPContext.new(request.request_method, request.url, get_http_headers(request.env), nil)
+      end
+
+      def report_push(env, maybe_ex)
+        ex =
+          if maybe_ex.is_a?(Exception)
+            maybe_ex
+          else
+            RuntimeError.new(maybe_ex.to_s)
+          end
+
+        @collector.report_with_context(env, ex)
+        @exporter.push_to_gateway(@pushgateway_address)
+      end
     end
   end
 end
